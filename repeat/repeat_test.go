5f3c643f271dd41a3a7a14fd8fa44166401d3aab
Toby <teeohbee@gmail.com>
2022-04-11 19:10:25 +0100

    Extend repeat to accept repeats arg

--- repeat/repeat_test.go
+++ repeat/repeat_test.go
@@ -1,18 +1,38 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	t.Run("repeating characters", func(t *testing.T) {
+		repeated := Repeat("a", 5)
+		expected := "aaaaa"
 
-	if repeated != expected {
-		t.Errorf("expected %q but got %q", expected, repeated)
-	}
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	})
+
+	t.Run("repeat characters a number of times", func(t *testing.T) {
+		repeated := Repeat("b", 2)
+		expected := "bb"
+
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	})
+}
+
+func ExampleRepeat() {
+	string := Repeat("c", 3)
+	fmt.Println(string)
+	// Output: ccc
 }
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 6)
 	}
 }
